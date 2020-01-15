7aeed22dd1d1875c4b31c8848aaf9ea4684a5019
Miguel Prieto <hello@mprieto.me>
2020-01-15 20:21:25 -0300

    Week 3: Nice String Task solution

--- Kotlin for Java Developers. Week 3/Nice String/Task/src/nicestring/NiceString.kt
+++ Kotlin for Java Developers. Week 3/Nice String/Task/src/nicestring/NiceString.kt
@@ -1,5 +1,9 @@
 package nicestring
 
 fun String.isNice(): Boolean {
-    TODO()
+    val condition1 = !contains("b[uae]".toRegex())
+    val condition2 = count { it in "aeiou" } >= 3
+    val condition3 = zipWithNext().any { (a, b) -> a == b }
+
+    return listOf(condition1, condition2, condition3).count { it } >= 2
 }
