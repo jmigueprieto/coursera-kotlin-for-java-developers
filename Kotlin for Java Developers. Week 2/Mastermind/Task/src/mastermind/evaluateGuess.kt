c12292c76952eb5197f7713b2d60bbd0e95d6759
Miguel Prieto <hello@mprieto.me>
2020-01-15 20:13:50 -0300

    Week 2 solution

--- Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -1,7 +1,28 @@
 package mastermind
 
+
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    TODO()
+    val secretArray = secret.toCharArray()
+    val guessArray = guess.toCharArray()
+    val rightPosition = 'R'
+    val wrongPosition = 'W'
+
+    secretArray.forEachIndexed { i, c ->
+        if (guessArray[i] == c) {
+            secretArray[i] = rightPosition
+            guessArray[i] = rightPosition
+        }
+    }
+
+    secretArray.forEachIndexed { i, c ->
+        if (c != 'R' && guessArray.contains(c)) {
+            secretArray[i] = wrongPosition
+            guessArray[guessArray.indexOf(c)] = wrongPosition
+        }
+    }
+
+    return Evaluation(secretArray.count { it == rightPosition },
+            secretArray.count { it == wrongPosition })
 }
