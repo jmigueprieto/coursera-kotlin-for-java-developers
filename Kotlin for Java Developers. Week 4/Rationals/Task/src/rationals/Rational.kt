36f4908835072c3516ad07f943a02e0c4c60d55b
Miguel Prieto <hello@mprieto.me>
2020-01-15 20:27:21 -0300

    Week 4 assignment initial commit

---
+++ Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
@@ -0,0 +1,37 @@
+package rationals
+
+
+
+fun main() {
+    val half = 1 divBy 2
+    val third = 1 divBy 3
+
+    val sum: Rational = half + third
+    println(5 divBy 6 == sum)
+
+    val difference: Rational = half - third
+    println(1 divBy 6 == difference)
+
+    val product: Rational = half * third
+    println(1 divBy 6 == product)
+
+    val quotient: Rational = half / third
+    println(3 divBy 2 == quotient)
+
+    val negation: Rational = -half
+    println(-1 divBy 2 == negation)
+
+    println((2 divBy 1).toString() == "2")
+    println((-2 divBy 4).toString() == "-1/2")
+    println("117/1098".toRational().toString() == "13/122")
+
+    val twoThirds = 2 divBy 3
+    println(half < twoThirds)
+
+    println(half in third..twoThirds)
+
+    println(2000000000L divBy 4000000000L == 1 divBy 2)
+
+    println("912016490186296920119201192141970416029".toBigInteger() divBy
+            "1824032980372593840238402384283940832058".toBigInteger() == 1 divBy 2)
+}
